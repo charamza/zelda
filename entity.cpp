7d20ce88be8393c7aaf98aa689715d177b216503
Honza Charamza <jenda60@gmail.com>
2017-06-06 12:09:41 +0200

    Úprava entity

--- entity.cpp
+++ entity.cpp
@@ -1,6 +1,7 @@
 #include "entity.h"
+#include "game.h"
 
-Entity::Entity(int x, int y, int width, int height)
+Entity::Entity(Game *game, int x, int y, int width, int height)
 {
     this->x = x;
     this->y = y;
@@ -9,7 +10,27 @@ Entity::Entity(int x, int y, int width, int height)
     this->speed = 1;
 }
 
+void Entity::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    this->draw(painter);
+}
+
+QRectF Entity::boundingRect() const
+{
+    return QRectF(0, 0, Tile::SIZE*WIDTH, Tile::SIZE*HEIGHT);
+}
+
+QPainterPath Entity::shape() const
+{
+    QPainterPath path;
+    path.addRect(boundingRect());
+    return path;
+}
+
 void Entity::updateMove()
 {
     // TODO: Dodělat logiku základního pohybu
+    this->x += dx*speed;
+    this->y += dx*speed;
 }
+
--- entity.h
+++ entity.h
@@ -2,14 +2,20 @@
 #define ENTITY_H
 
 #include <QPainter>
+#include <QGraphicsItem>
 
-class Entity
+class Game;
+
+class Entity : public QGraphicsItem
 {
 public:
-    Entity(int x, int y, int width, int height);
+    Entity(Game *game, int x, int y, int width, int height);
+    QRectF boundingRect() const;
+    QPainterPath shape() const;
+	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 
     virtual void update() const = 0;
-    virtual void draw() const = 0;
+    virtual void draw(QPainter *gl) const = 0;
 
     void updateMove();
 
@@ -17,6 +23,9 @@ public:
     int width, height;
     float speed;
     int dx = 0, dy = 0; // DX -1 = Levo, DX 1 = Pravo, DY -1 = nahoru, DY 1 = dolu
+	
+private:
+	Game *game;
 };
 
 #endif // ENTITY_H
