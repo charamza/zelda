b12176ddaa453b0d1aeb47ca85dc98fde26fea5c
Unknown <gamesforliveczech@gmail.com>
2017-06-12 18:41:06 +0200

    Player animace done.

--- player.cpp
+++ player.cpp
@@ -8,7 +8,24 @@ Player::Player(Game *game, int x, int y) : Entity(game, x, y, 32, 48)
 
 void Player::draw(QPainter *gl)
 {
-    gl->drawImage(boundingRect(), game->resource->player, QRect(16*(game->STEPS/15%4), 6, 16, 24));
+
+    QRect texture;
+    if(dy == 1){
+        texture = QRect(16*(game->STEPS/15%4), 6, 16, 24);
+    }
+    if(dy == -1){
+        texture = QRect(16*(game->STEPS/15%4), 69, 16, 24);
+    }
+    if(dx == 1){
+        texture = QRect(16*(game->STEPS/15%4), 38, 16, 22);
+    }
+    if(dx == -1){
+        texture = QRect(16*(game->STEPS/15%4), 102, 16, 22);
+    }
+    if(dx == 0 && dy==0){
+        texture = QRect(16*(game->STEPS/15%1), 6, 16, 24);
+    }
+    gl->drawImage(boundingRect(), game->resource->player, texture);
 }
 
 void Player::update()
