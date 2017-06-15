18cf7ad82357419af9ce601b6c79a7a9c690fafb
Honza Charamza <jenda60@gmail.com>
2017-06-15 10:52:02 +0200

    Smazání přebytečného adresáře

--- Zelda/basictile.cpp
+++
@@ -1,9 +0,0 @@
-#include "basictile.h"
-
-BasicTile::BasicTile(Game *game, int x, int y, int id) : Tile(game, x, y, id)
-{
-    this->game = game;
-    this->x = x;
-    this->y = y;
-    this->id = id;
-}
